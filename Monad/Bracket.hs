0ee983af5181d4558ab14a3fdffdcb534b68c4f6
Duncan Burke <duncankburke@gmail.com>
2014-11-03 19:55:32 +1100

    working

--- Monad/Bracket.hs
+++ Monad/Bracket.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE FlexibleInstances, RankNTypes, MultiParamTypeClasses, ConstraintKinds #-}
-
 module Monad.Bracket (
   BracketT,
   bracketC,
@@ -13,23 +11,15 @@ import Monad.Try
 import Control.Monad.Trans.Cont (ContT(ContT), runContT)
 import Control.Monad.Trans.Class
 
--- type BracketT r m a = (MonadTry m) => ContT r m (m a)
+type BracketT_ m = ContT () m
 
--- bracketC :: (MonadTry m) => m a -> (a -> m b) -> BracketT r m a
--- bracketC init final = _ $ bracket init final
+type BracketT m = BracketT_ m ()
 
--- --bracketC_ :: (MonadTry m) => m a -> m b -> BracketT r m a
--- bracketC_ init final = bracketC init $ const final
-
--- runBracketC m = runContT m id
-
-type BracketT r m a = ContT r m a
-
-bracketC :: (MonadTry m) => m a -> (a -> m c) -> BracketT r m a
+bracketC :: (MonadTry m) => m a -> (a -> m b) -> BracketT_ m a
 bracketC start final = ContT $ bracket start final
 
-bracketC_ :: (MonadTry m) => m a -> m c -> BracketT r m a
+bracketC_ :: (MonadTry m) => m a -> m b -> BracketT_ m a
 bracketC_ start final = bracketC start $ const final
 
-runBracketC :: (MonadTry m) => BracketT a m (m a) -> m a
-runBracketC m = runContT m id
+runBracketC :: (MonadTry m) => BracketT_ m a -> m ()
+runBracketC b = runContT b (\_ -> return ())
