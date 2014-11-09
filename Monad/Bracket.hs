02c7643aa54490a616b93cb4bb97856cfa2bd919
Duncan Burke <duncankburke@gmail.com>
2014-11-10 00:37:02 +1100

    allowed returning of non-unit values

--- Monad/Bracket.hs
+++ Monad/Bracket.hs
@@ -11,15 +11,13 @@ import Monad.Try
 import Control.Monad.Trans.Cont (ContT(ContT), runContT)
 import Control.Monad.Trans.Class
 
-type BracketT_ m = ContT () m
+type BracketT m = ContT () m
 
-type BracketT m = BracketT_ m ()
-
-bracketC :: (MonadTry m) => m a -> (a -> m b) -> BracketT_ m a
+bracketC :: (MonadTry m) => m a -> (a -> m b) -> BracketT m a
 bracketC start final = ContT $ bracket start final
 
-bracketC_ :: (MonadTry m) => m a -> m b -> BracketT_ m a
+bracketC_ :: (MonadTry m) => m a -> m b -> BracketT m a
 bracketC_ start final = bracketC start $ const final
 
-runBracketT :: (MonadTry m) => BracketT_ m a -> m ()
+runBracketT :: (MonadTry m) => BracketT m a -> m ()
 runBracketT b = runContT b (\_ -> return ())
