a99e3413779c209c560b5ddcd0cb41c3ee49a6a5
Duncan Burke <duncankburke@gmail.com>
2014-11-13 01:30:54 +1100

    Added inference of base type for additional transformations.

--- Monad/Bracket.hs
+++ Monad/Bracket.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Monad.Bracket (
   BracketT,
@@ -17,15 +20,33 @@ import Control.Monad.Trans.Class
 
 import Control.Monad.Lift
 
+#if MIN_VERSION_mmorph(1, 0, 1)
+import           Control.Monad.Trans.Compose (ComposeT (ComposeT))
+#endif
+
 type BracketT m a = (MonadTry m, Monad m) => ContT () m a
 
 class (Monad m) => MonadBracket m where
-  bracketC :: (Monad n, MonadTrans t, m ~ (t n)) => n a -> (a -> n b) -> m a
-  bracketC_ :: (Monad n, MonadTrans t, m ~ (t n)) => n a -> n b -> m a
+  type BracketInner m :: * -> *
+  bracketC :: (Monad n, n ~ BracketInner m) => n a -> (a -> n b) -> m a
+  bracketC_ :: (Monad n, n ~ BracketInner m ) => n a -> n b -> m a
   bracketC_ start final = bracketC start $ const final
 
-instance (MonadTry m) => MonadBracket (ContT () m) where
-  bracketC start final = ContT $ bracket start final
+#if MIN_VERSION_mmorph(1, 0, 1)
+instance (MonadBracket (f (g m)),
+          BracketInner (f (g m)) ~ BracketInner (ComposeT f g m))
+         => MonadBracket (ComposeT f g m) where
+  bracketC start final = ComposeT (bracketC start final)
+#endif
+
+class (Monad m, Monad n) => MonadBracket' m n where
+  bracketC' :: n a -> (a -> n b) -> m a
+
+instance (MonadTry n) => MonadBracket' (ContT () n) n where
+  bracketC' start final = ContT $ bracket start final
+
+instance (MonadBracket' m n, n ~ BracketInner m) => MonadBracket m where
+  bracketC = bracketC'
 
 runBracketT :: (MonadTry m) => BracketT m a -> m ()
 runBracketT b = runContT b (\_ -> return ())
